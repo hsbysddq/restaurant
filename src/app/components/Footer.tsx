23497342bcd0c031059595cd04d0ff3bad85c691
hsbysddq <hsby15sddq@gmail.com>
2023-09-10 17:04:02 +0700

    Create featured responsive design from mobile to desktop

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40  text-red-500 flex items-center justify-between">
       <Link href="/" className="font-bold text-xl">
-        PATROBASs
+        PATROBAS
       </Link>
       <p>ALL RIGHTS RESERVED.</p>
     </div>
