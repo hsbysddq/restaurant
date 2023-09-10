757d71a7bf541d03f897cf32612a0e512a4f34a9
hsbysddq <hsby15sddq@gmail.com>
2023-09-10 10:14:01 +0700

    Create responsive navbar design and logic  from mobile to desktop screens

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,46 @@
+import Link from "next/link"
 import React from "react"
+import Menu from "./Menu"
+import CartIcon from "./CartIcon"
+import Image from "next/image"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  const user = false
+
+  return (
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:24 lg:px-20 xl:40">
+      {/* LEFT LINK */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+
+      {/* LOGO */}
+      <div className="text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/">Patrobas</Link>
+      </div>
+
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <Menu />
+      </div>
+
+      {/* RIGHT LINK */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20}></Image>
+          <span>+62 838 341</span>
+        </div>
+        {!user ? (
+          <Link href="/">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
