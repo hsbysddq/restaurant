5a70870a147e78db1d7285be18536b203b087225
hsbysddq <hsby15sddq@gmail.com>
2023-09-13 15:58:56 +0700

    Create responsive design product

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,28 @@
+import { singleProduct } from "@/data"
+import Image from "next/image"
 import React from "react"
 
 const SingleProductPage = () => {
-  return <div>SingleProductPage</div>
+  return (
+    <div className="p-4 lg:p-20 xl:p-40 h-screen flex flex-col justify-around text-red-500 md:flex-row">
+      {/* IMAGE CONTAINER */}
+      {singleProduct.img && (
+        <div className="relative">
+          <Image
+            src={singleProduct.img}
+            alt=""
+            className="object-contain"
+            fill
+          />
+        </div>
+      )}
+      {/* TEXT CONTAINER */}
+      <div className="">
+        <h1>{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+      </div>
+    </div>
+  )
 }
 
 export default SingleProductPage
