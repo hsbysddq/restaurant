e16c87bd775680b7739797cb6d0840262d69fadf
hsbysddq <hsby15sddq@gmail.com>
2023-09-18 21:16:20 +0700

    Edit tampilan

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -8,7 +8,7 @@ const SingleProductPage = () => {
     <div className="p-4 lg:p-20 xl:p-40 h-screen flex flex-col justify-around text-red-500 md:flex-row">
       {/* IMAGE CONTAINER */}
       {singleProduct.img && (
-        <div className="relative">
+        <div className="relative w-full h-1/2">
           <Image
             src={singleProduct.img}
             alt=""
@@ -18,8 +18,8 @@ const SingleProductPage = () => {
         </div>
       )}
       {/* TEXT CONTAINER */}
-      <div className="">
-        <h1>{singleProduct.title}</h1>
+      <div className="h-1/2 flex flex-col gap-4">
+        <h1 className="text-3xl font-bold uppercase">{singleProduct.title}</h1>
         <p>{singleProduct.desc}</p>
         <Price
           price={singleProduct.price}
