9c3ddb333dc49f9025d656d482273cc2d700f210
hsbysddq <88543652+hsbysddq@users.noreply.github.com>
2023-09-20 04:32:50 +0700

    Edit tampilan

--- src/app/components/Price.tsx
+++ src/app/components/Price.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+"use client"
+import React, { useEffect, useState } from "react"
 import { types } from "util"
 
 type Props = {
@@ -7,15 +8,30 @@ type Props = {
   options?: { title: string; additionalPrice: number }[]
 }
 const Price = ({ price, id, options }: Props) => {
+  const [total, setTotal] = useState(price)
+  const [quantity, setQuantity] = useState(1)
+  const [selected, setSelected] = useState(0)
+
+  useEffect(() => {
+    setTotal(
+      quantity * (options ? price + options[selected].additionalPrice : price)
+    )
+  }, [quantity, selected, options, price])
+
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-2xl font-bold">${price.toFixed(2)}</h2>
+      <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
       {/* OPTIONS CONTAINER */}
       <div className="flex gap-4">
-        {options?.map((option) => (
+        {options?.map((option, index) => (
           <button
             key={option.title}
             className="p-2 ring-1 ring-red-400 rounded-md"
+            style={{
+              background: selected === index ? "rgb(248 113 113" : "white",
+              color: selected === index ? "white" : "red",
+            }}
+            onClick={() => setSelected(index)}
           >
             {option.title}
           </button>
@@ -28,9 +44,17 @@ const Price = ({ price, id, options }: Props) => {
         <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
           <span>Quantity</span>
           <div className="flex gap-4 items-center">
-            <button>{"<"}</button>
-            <span>1</span>
-            <button>{">"}</button>
+            <button
+              onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}
+            >
+              {"<"}
+            </button>
+            <span>{quantity}</span>
+            <button
+              onClick={() => setQuantity((prev) => (prev < 9 ? prev + 1 : 9))}
+            >
+              {">"}
+            </button>
           </div>
         </div>
         {/* CART BUTTON */}
